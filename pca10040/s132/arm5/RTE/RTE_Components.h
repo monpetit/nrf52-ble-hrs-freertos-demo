644e5864035308bafc7970539171f6b46943b550
Dogma™ <monpetit@gmail.com>
2015-12-11 14:08:57 +0900

    initial commit

---
+++ pca10040/s132/arm5/RTE/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ble_app_hrs_freertos_s132_pca10040' 
+ * Target:  'nrf52_ble_app_hrs_freertos_s132' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+#define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define GPIOTE_ENABLED
+  #define GPIOTE_CONFIG_NUM_OF_LOW_POWER_EVENTS
+#define SOFTDEVICE_PRESENT
+#define UART0_ENABLED
+#define configUSE_TIMERS
+
+#endif /* RTE_COMPONENTS_H */
